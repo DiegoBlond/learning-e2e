41e56cc1f1a037fe54a245b5fb51bc17797b5407
Your Name <you@example.com>
2017-02-23 23:00:03 -0300

    new calc functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,23 @@ export class AppComponent {
     title = 'app works!';
     valor1: number = 0;
     valor2: number = 0;
+    valor3: number = 0;
     result;
 
     divide() {
-        this.result = this.valor1 / this.valor2 || 'não deu';
+        if (this.valor1 || this.valor2 || this.valor3) {
+            this.result = 'não deu';
+        }
+        else {
+            this.result = this.valor1 / this.valor2 / this.valor3 || 'não deu';
+        }
+    }
+
+    multiplica() {
+        this.result = this.valor1 * this.valor2 * this.valor3;
+    }
+
+    soma() {
+        this.result = this.valor1 + this.valor2 + this.valor3;
     }
 }

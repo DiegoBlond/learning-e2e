b1c3bba974749fe8b884838c511daaeaea3a37d8
Your Name <you@example.com>
2017-02-23 22:45:05 -0300

    testing division funcionality

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,7 +13,30 @@ describe('teste-angular-cli App', function() {
   });
 
   it('should change title when I edit the input field', () => {
-      page.editInputFieldWith("new text");
+      page.editInputFieldWith('new text');
       expect(page.getParagraphText()).toEqual('new text');
   });
+
+  it('should give error message when we dont inform any number', () => {
+    page.clickButton();
+    expect(page.getResultText()).toEqual('não deu');
+  });
+
+  it('should give right result of a division', () => {
+    page.editFirstNumber('10');
+    page.editSecondNumber('5');
+    page.clickButton();
+    expect(page.getResultText()).toEqual('2');
+  });
+
+  it('should not write anything on the first input when the user tries to write letters', () => {
+    page.editFirstNumber('abc');
+    expect(page.getFirstInputValue()).toEqual('');
+  });
+
+  it('should not write anything on the second input when the user tries to write letters', () => {
+    page.editSecondNumber('dfg');
+    expect(page.getSecondInputValue()).toEqual('');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -14,4 +14,33 @@ export class TesteAngularCliPage {
       input.clear();
       input.sendKeys(newText);
   }
+
+  clickButton() {
+    var button = element(by.css('button'));
+    button.click();
+  }
+
+  editFirstNumber(newNumber1) {
+    var firstNumberInput = element(by.css('.qa-first-field'));
+    firstNumberInput.clear();
+    firstNumberInput.sendKeys(newNumber1);
+  }
+
+  editSecondNumber(newNumber2 ){
+    var secondNumberInput = element(by.css('.qa-second-field'));
+    secondNumberInput.clear();
+    secondNumberInput.sendKeys(newNumber2);
+  }
+
+  getResultText() {
+    return element(by.css('.qa-result')).getText();
+  }
+
+  getFirstInputValue() {
+    return element(by.css('.qa-first-field')).getAttribute('value');
+  }
+
+  getSecondInputValue() {
+    return element(by.css('.qa-second-field')).getAttribute('value');
+  }
 }
